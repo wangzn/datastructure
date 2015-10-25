765a355939d72907cf71ee2654c1be63be3f46c7
wangzn <wzpzning@gmail.com>
2015-10-25 15:57:20 +0800

    add stack

---
+++ src/stack/stack.go
@@ -0,0 +1,50 @@
+package stack
+
+import (
+	"errors"
+	"sync"
+)
+
+type Stack struct {
+	data   []interface{}
+	length int
+	sync.Mutex
+}
+
+func New() *Stack {
+	return &Stack{
+		data:   make([]interface{}, 0),
+		length: 0,
+	}
+}
+
+func (s *Stack) IsEmpty() bool {
+	return s.length == 0
+}
+
+func (s *Stack) Length() int {
+	s.Lock()
+	defer s.Unlock()
+	return s.length
+}
+
+func (s *Stack) Push(i interface{}) {
+	s.Lock()
+	defer s.Unlock()
+	data := make([]interface{}, 1)
+	data[0] = i
+	s.data = append(data, s.data...)
+	s.length++
+}
+
+func (s *Stack) Pop() (interface{}, error) {
+	if s.Length() == 0 {
+		return nil, errors.New("Stack is empty")
+	}
+	s.Lock()
+	defer s.Unlock()
+	i := s.data[0]
+	s.data = s.data[1:]
+	s.length--
+	return i, nil
+}
---
+++ src/stack/stack_test.go
@@ -0,0 +1,30 @@
+package stack
+
+import (
+	"testing"
+)
+
+func TestMain(t *testing.T) {
+	s := New()
+	s.Push(1)
+	s.Push(2)
+	s.Push(3)
+	s.Push(4)
+	if s.Length() != 4 {
+		t.Error("Stack length error after push")
+	}
+	v, err := s.Pop()
+	if v != 4 {
+		t.Error("Stack pop value error")
+	}
+	if s.Length() != 3 {
+		t.Error("length error after pop")
+	}
+	v, err = s.Pop()
+	v, err = s.Pop()
+	v, err = s.Pop()
+	v, err = s.Pop()
+	if v != nil || err == nil {
+		t.Error("Pop should be invalid")
+	}
+}
