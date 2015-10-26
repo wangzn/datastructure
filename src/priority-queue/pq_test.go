1d54d550b763e6261dec99b2b843e185b63ac65f
wangzn <wzpzning@gmail.com>
2015-10-26 18:38:22 +0800

    add more test suit

--- src/priority-queue/pq_test.go
+++ src/priority-queue/pq_test.go
@@ -16,4 +16,23 @@ func TestMain(t *testing.T) {
 	if pq.Length() != 4 {
 		t.Error("Length error after insert")
 	}
+	v, pri := pq.Extract()
+	if v != "a" || pri != 1 {
+		t.Error("Extract value fail", v, pri)
+	}
+	v, pri = pq.Extract()
+	if v != "b" || pri != 2 {
+		t.Error("Extract value fail", v, pri)
+	}
+	v, pri = pq.Extract()
+	if v != "c" || pri != 3 {
+		t.Error("Extract value fail", v, pri)
+	}
+	v, pri = pq.Extract()
+	if v != "d" || pri != 4 {
+		t.Error("Extract value fail", v, pri)
+	}
+	if pq.Length() != 0 {
+		t.Error("Length error after clean up")
+	}
 }
