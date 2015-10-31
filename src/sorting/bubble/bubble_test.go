c008f11576ca587a377ca612a5502c044861a27d
wangzn <wzpzning@gmail.com>
2015-10-31 16:42:15 +0800

    update test, add benchmark

--- src/sorting/bubble/bubble_test.go
+++ src/sorting/bubble/bubble_test.go
@@ -1,21 +1,39 @@
 package bubble
 
 import (
-	"fmt"
 	"testing"
+
+	"github.com/wangzn/datastructure/src/sorting/util"
 )
 
-func TestBubble(t *testing.T) {
-	BubbleSort(make([]int, 0))
-	b := BubbleSort([]int{1})
-	a := []int{4, 1, 7, 0, 3, 2, 6, 5}
-	b = BubbleSort(a)
-	for i := 0; i < 7; i++ {
-		if b[i] != i {
-			t.Error("Sort error")
-			fmt.Println(b)
-			return
-		}
+func TestBubbleSort(t *testing.T) {
+	BubbleSort(util.GenArray(0))
+	a := BubbleSort(util.GenArray(100))
+	c := util.CheckArray(a)
+	if c != 0 {
+		t.Error("Sort array fail, first fail index", c)
 	}
+}
 
+func benchmarkBubbleSort(n int, b *testing.B) {
+	a := util.GenArray(n)
+	for i := 0; i < b.N; i++ {
+		BubbleSort(a)
+	}
+}
+
+func BenchmarkBubbleSort100(b *testing.B) {
+	benchmarkBubbleSort(100, b)
+}
+
+func BenchmarkBubbleSort1000(b *testing.B) {
+	benchmarkBubbleSort(1000, b)
+}
+
+func BenchmarkBubbleSort10000(b *testing.B) {
+	benchmarkBubbleSort(10000, b)
+}
+
+func BenchmarkBubbleSort100000(b *testing.B) {
+	benchmarkBubbleSort(100000, b)
 }
