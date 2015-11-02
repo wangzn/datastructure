b2021a5596d07c21ce24019c2d84d5694f6a523c
wangzn <wzpzning@gmail.com>
2015-11-02 21:41:17 +0800

    add benchmark

--- src/eratosthenes-sieve/sieve_test.go
+++ src/eratosthenes-sieve/sieve_test.go
@@ -9,3 +9,19 @@ func TestMain(t *testing.T) {
 	ret := Sieve(100)
 	fmt.Println(ret)
 }
+
+func benchmarkGetAllPrimesTo(n int, b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Sieve(n)
+	}
+}
+
+func BenchmarkGetAllPrimesTo100(b *testing.B) {
+	benchmarkGetAllPrimesTo(100, b)
+}
+func BenchmarkGetAllPrimesTo10000(b *testing.B) {
+	benchmarkGetAllPrimesTo(10000, b)
+}
+func BenchmarkGetAllPrimesTo1000000(b *testing.B) {
+	benchmarkGetAllPrimesTo(1000000, b)
+}
